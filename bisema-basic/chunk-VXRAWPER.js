61997542b78f53a7127d483cbf4a55350a759260
Semegne <63513161+semegne@users.noreply.github.com>
2025-02-28 09:08:41 -0500

    Delete bisema-basic directory

--- bisema-basic/chunk-VXRAWPER.js
+++
@@ -1,1 +0,0 @@
-import{d as o}from"./chunk-4CLCTAJ7.js";var t=[{path:"login",loadComponent:()=>o(void 0,null,function*(){return(yield import("./chunk-LLIGWR7J.js")).LoginComponent})}];export{t as routes};
