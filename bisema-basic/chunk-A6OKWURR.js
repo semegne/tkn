73429688c65de22458d764632288c5525cd75d7c
Semegne <semegne.tafesse@bisema.com>
2025-02-28 09:11:20 -0500

    Feb280911AM

---
+++ bisema-basic/chunk-A6OKWURR.js
@@ -0,0 +1,1 @@
+import{d as o}from"./chunk-4CLCTAJ7.js";var t=[{path:"login",loadComponent:()=>o(void 0,null,function*(){return(yield import("./chunk-KAJ72FOO.js")).LoginComponent})}];export{t as routes};
