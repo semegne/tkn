40cdea141bab681635236142bc66d8c84670fe88
Semegne <semegne.tafesse@bisema.com>
2025-03-03 11:47:54 -0500

    March031147AM

---
+++ bisema-basic/chunk-OOXUS4GZ.js
@@ -0,0 +1,1 @@
+import{d as o}from"./chunk-4CLCTAJ7.js";var t=[{path:"",loadComponent:()=>o(void 0,null,function*(){return(yield import("./chunk-5USC6ELX.js")).CartComponent})}];export{t as routes};
