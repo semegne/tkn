40973e95f710f59c879ca70b219330b434991a5c
Semegne <63513161+semegne@users.noreply.github.com>
2025-03-03 11:45:04 -0500

    Delete bisema-basic directory

--- bisema-basic/chunk-HUVCVB46.js
+++
@@ -1,1 +0,0 @@
-import{d as o}from"./chunk-4CLCTAJ7.js";var t=[{path:"",loadComponent:()=>o(void 0,null,function*(){return(yield import("./chunk-HDHXONAG.js")).CartComponent})}];export{t as routes};
